1b66a685dea600749a12fd4b3456ce931800e5dd
MarcosSWeis <marcosweis01@gmail.com>
2021-11-05 14:55:56 -0300

    subirndo proyecto optica a grupo-8

---
+++ proyecto-optica/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static(path.join(__dirname, "/public")));
+app.listen(3000, () => console.log("El server esta funcionando"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+// si hago estatica la carpeta public entonces desde el html puedo acceder con / y siempre con barra
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"));
+});
+
+app.get("/ingresa", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"));
+});
