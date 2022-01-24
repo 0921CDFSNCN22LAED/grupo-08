7506acca5302a55f4dea2922bb7768af5f75981b
Esteban Larcher <estebanlarcher098@gmail.com>
2022-01-24 16:53:14 -0300

    associations orders & car

--- proyecto-optica/database/models/user.js
+++ proyecto-optica/database/models/user.js
@@ -1,52 +1,51 @@
 const sequelize = require("sequelize");
 const dataTypes = sequelize.DataTypes;
 module.exports = (sequelize) => {
-  const alias = "User"; // esto debería estar en singular
-  const cols = {
-    name: dataTypes.STRING,
+	const alias = "User"; // esto debería estar en singular
+	const cols = {
+		name: dataTypes.STRING,
 
-    lastName: dataTypes.STRING,
+		lastName: dataTypes.STRING,
 
-    country: dataTypes.STRING,
+		country: dataTypes.STRING,
 
-    dateBirth: dataTypes.DATE,
+		dateBirth: dataTypes.DATE,
 
-    country: dataTypes.STRING,
+		country: dataTypes.STRING,
 
-    email: dataTypes.STRING,
+		email: dataTypes.STRING,
 
-    password: dataTypes.STRING,
+		password: dataTypes.STRING,
 
-    confirmPassword: dataTypes.STRING,
+		confirmPassword: dataTypes.STRING,
 
-    streetAddress: dataTypes.STRING,
+		streetAddress: dataTypes.STRING,
 
-    buySell: dataTypes.STRING,
+		buySell: dataTypes.STRING,
 
-    favoriteCategory: dataTypes.STRING,
+		favoriteCategory: dataTypes.STRING,
 
-    avatar: dataTypes.STRING,
-  };
-  const config = {
-    timestamps: true,
-    createdAt: "created_at",
-    updatedAt: "updated_at",
-    deletedAt: false,
-  };
-  const User = sequelize.define(alias, cols, config);
+		avatar: dataTypes.STRING,
+	};
+	const config = {
+		timestamps: true,
+		createdAt: "created_at",
+		updatedAt: "updated_at",
+		deletedAt: false,
+	};
+	const User = sequelize.define(alias, cols, config);
 
-  User.associate = function (modelos) {
-    User.belongsTo(modelos.Car, {
-      as: "car",
-      foreignKey: "user_id",
-    });
-    User.belongsToMany(modelos.Orders, {
-      as: "orders",
-      through: "order_detail",
-      foreignKey: "user_id",
-      otherId: "order_id",
-    });
-  };
-  return User;
+	User.associate = function (modelos) {
+		User.belongsTo(modelos.Car, {
+			as: "car",
+			foreignKey: "user_id",
+		});
+		User.belongsToMany(modelos.Orders, {
+			as: "orders",
+			through: "order_detail",
+			foreignKey: "user_id",
+			otherId: "order_id",
+		});
+	};
+	return User;
 };
-
