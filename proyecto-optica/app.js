ae4f2c030c8b1af7a9135c28792f40124d643997
Esteban Larcher <estebanlarcher098@gmail.com>
2022-01-31 23:11:53 -0300

    comit esteban

--- proyecto-optica/app.js
+++ proyecto-optica/app.js
@@ -19,11 +19,11 @@ app.use(express.json()); // LO CAPTURADO DEL FORMULARIO QUE ME LO GUARDE EN FORM
 app.set("view engine", "ejs");
 // ********  MIDDLEWARES DE APLICATION ********//
 app.use(
-  session({
-    secret: "princesa bebe",
-    resave: false,
-    saveUninitialized: true,
-  })
+	session({
+		secret: "princesa bebe",
+		resave: false,
+		saveUninitialized: true,
+	})
 );
 app.use(cookies());
 app.use(rememberUserCookieMiddleware);
--- proyecto-optica/database/migrations/20220118044235-create-product.js
+++ proyecto-optica/database/migrations/20220118044235-create-product.js
@@ -1,41 +1,50 @@
 "use strict";
 
+const { INTEGER } = require("sequelize");
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("products", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      shortDescription: {
-        type: Sequelize.STRING(500),
-      },
-      longDescriptions: {
-        type: Sequelize.TEXT,
-      },
-      prescription_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "prescriptions",
-          key: "id",
-        },
-      },
-      createdAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("products");
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("products", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+			},
+			shortDescription: {
+				type: Sequelize.STRING(500),
+			},
+			longDescriptions: {
+				type: Sequelize.TEXT,
+			},
+			prescription_id: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: "prescriptions",
+					key: "id",
+				},
+			},
+			size_id: {
+				type: Sequelize.INTEGER,
+				references: {
+					model: "sizes",
+					key: "id",
+				},
+			},
+			createdAt: {
+				allowNull: true,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: true,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("products");
+	},
 };
