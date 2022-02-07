c08ce482701acebbbab4406af96ee3b6c9b2f85c
MarcosSWeis <marcosweis01@gmail.com>
2022-02-07 15:51:29 -0300

    creacion de api router y api controller

---
+++ proyecto-optica/src/controllers/api/product-api-controller.js
@@ -0,0 +1,45 @@
+const db = require("../../database/models");
+const Product = db.Product;
+module.exports = {
+  list: async (req, res) => {
+    const products = await Product.findAll({
+      include: ["image", "price", "size", "material", "color"],
+    });
+    let status;
+    if (products) {
+      status = 200;
+    } else {
+      status = 404;
+    }
+    let response = {
+      meta: {
+        status: status,
+        total: products.length,
+        url: "/api/products",
+      },
+      data: products,
+    };
+    res.json(response);
+  },
+
+  detail: async (req, res) => {
+    const product = Product.findByPk(req.params.id, {
+      include: ["image", "price", "size", "material", "color"],
+    });
+    let status;
+    if (product) {
+      status = 200;
+    } else {
+      status = 404;
+    }
+    let response = {
+      meta: {
+        status: status,
+        total: 1,
+        url: "/api/product/id",
+      },
+      data: product,
+    };
+    res.json(response);
+  },
+};
---
+++ proyecto-optica/src/routers/api/product-api-router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const productApiController = require("../../controllers/api/product-api-controller");
+const router = express.Router();
+
+router.get("/", productApiController.list);
+router.get("/id", productApiController.detail);
+
+module.exports = router;
