7f3c78894394f6243c91612b1ab11db20d7ca7f3
MarcosSWeis <marcosweis01@gmail.com>
2022-02-07 20:31:03 -0300

    cambio de nombre para conectarse ala db

--- proyecto-optica/src/database/config/config.js
+++ proyecto-optica/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: null,
-    database: "proyecto_optica_prueba",
+    database: "proyecto_optica",
     host: "127.0.0.1",
     dialect: "mysql",
   },
