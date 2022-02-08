5514df5a499424bb30926a366042ad21a9e0eb2a
MarcosSWeis <marcosweis01@gmail.com>
2022-02-07 21:05:57 -0300

    delete

--- proyecto-optica/public/js/products/product-edit.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", function () {
-  const smallCarrusel = document.querySelectorAll(".small-carrusel img");
-  console.log(smallCarrusel);
-  smallCarrusel.addEventListener("click", function (e) {
-    console.log(this);
-    this.style.backgroundColor = "red";
-  });
-});
