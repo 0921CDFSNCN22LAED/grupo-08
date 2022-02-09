ee23593aed0b394b2dbefd6ec8098ec29de0c4b2
Marcos Weis <marcosweis01@gmail.com>
2022-02-09 18:24:21 -0300

    detalles de profile

--- proyecto-optica/public/js/user/user-profile.js
+++ proyecto-optica/public/js/user/user-profile.js
@@ -2,15 +2,21 @@ window.onload = function () {
   //   const body = document.querySelector("body");
   //   body.style.backgroundColor = "red";
   const containerForm = document.getElementById("containerForm");
-  containerForm.classList.add("formNone");
+  containerForm.classList.add("none");
+  const btnEnviar = document.getElementById("btnEnviar");
+  btnEnviar.classList.add("none");
+
   const btnEdit = document.getElementById("btnEdit");
   btnEdit.addEventListener("click", () => {
-    const formNone = containerForm.classList.contains("formNone");
-    console.log(formNone);
-    if (formNone) {
-      containerForm.classList.replace("formNone", "formBlock");
-    } else {
-      containerForm.classList.replace("formBlock", "formNone");
-    }
+    const formNone = containerForm.classList.contains("none");
+    const enviarNone = btnEnviar.classList.contains("none");
+    //if ternario
+    formNone
+      ? containerForm.classList.replace("none", "block")
+      : containerForm.classList.replace("block", "none");
+    //if ternario
+    enviarNone
+      ? btnEnviar.classList.replace("none", "block")
+      : btnEnviar.classList.replace("block", "none");
   });
 };
