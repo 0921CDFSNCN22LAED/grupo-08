7f9d50eb5bdf923298a095d1c9c19935005901cc
Marcos Weis <marcosweis01@gmail.com>
2022-02-18 19:25:28 -0300

    database config rename vision

--- website/src/database/config/config.js
+++ website/src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: "root",
     password: null,
-    database: "proyecto_optica",
+    database: "vision",
     host: "127.0.0.1",
     dialect: "mysql",
   },
