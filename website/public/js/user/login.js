6670ce6a69bbae4bc586722d257949511bd73808
segundogosio <segundogosio@gmail.com>
2022-02-23 16:54:02 -0300

    register front

--- website/public/js/user/login.js
+++ website/public/js/user/login.js
@@ -1,7 +1,10 @@
 window.addEventListener("load", function(){
 
+    let error = []  
     let email = document.querySelector("#email")
     let password = document.querySelector("#password")
+    let 
     
+
 
 })
--- website/public/js/user/register.js
+++ website/public/js/user/register.js
@@ -1,26 +1,29 @@
 window.addEventListener("load", function(){
 
     let error = []    
-    let name = document.querySelector("#name")
-    let lastName = document.querySelector("#lastName")
-    let email = document.querySelector("#email")
-    let password = document.querySelector("#password")
-    let avatar = document.querySelector("#avatar")
-    
+    let form = document.getElementById("formRegister")
+    let name = document.getElementById("name")
+    let lastName = document.getElementById("lastName")
+    let email = document.getElementById("email")
+    let password = document.getElementById("password")
+    let avatar = document.getElementById("avatar")
+    let erroresName = document.getElementById("erroresName")
+    console.log(form)
+    console.log(name)
+
+    form.addEventListener("submit", function(event){
+
     if (name.value == ""){
-        error.push("Debe tener mas de 2 caracteres")
+        error.push("a")
+        erroresName.innerText = "Debe llenar por lo menos 2 caracteres. front"
     }
-    if (error.length > 1){
-        event.preventDefault()
-    }
-
-    if (lastName.value == ""){
-        error.push("Debe tener mas de 2 caracteres")
-    }
-    if (error.length > 1){
+    console.log(error)
+    if (error.length != 0){
         event.preventDefault()
     }
 
     
 
+})
+    
 })
