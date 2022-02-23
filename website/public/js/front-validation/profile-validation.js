ff9cae47933e34068f165010209cadaa02d0a6d1
Esteban Larcher <estebanlarcher098@gmail.com>
2022-02-23 15:56:34 -0300

    validation front userProfile

--- website/public/js/front-validation/profile-validation.js
+++ website/public/js/front-validation/profile-validation.js
@@ -2,17 +2,25 @@ window.addEventListener("load", function () {
     alert("hola");
     const formValidation = document.querySelector("#formProfile");
     const blankName = document.querySelector("#profileName");
-    const ulErrors = document.querySelector("#errores");
+    const blankLastName = document.querySelector("#profileLastName");
+    const errorName = document.querySelector("#errorName");
+    const errorLastName = document.querySelector("#errorLastName");
 
     formValidation.addEventListener("submit", function (e) {
         const errors = [];
-        console.log(blankName);
 
         if (blankName.value == "") {
-            errors.push("El nombre no puede estar vacio");
+            errors.push("");
         }
         if (errors.length > 0) {
-            ulErrors.innerText = "tenes que llenar este campo gilaso culea";
+            errorName.innerText = "El nombre debe estar lleno";
+            e.preventDefault();
+        }
+        if (blankLastName.value == "") {
+            errors.push("");
+        }
+        if (errors.length > 0) {
+            errorLastName.innerText = "El apellido debe estar lleno";
             e.preventDefault();
         }
     });
