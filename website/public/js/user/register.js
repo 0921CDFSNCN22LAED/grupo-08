1741399994dee9f21e49a372a74a5057d702f329
segundogosio <segundogosio@gmail.com>
2022-02-23 15:35:33 -0300

    react-router-dom

--- website/public/js/user/register.js
+++ website/public/js/user/register.js
@@ -1,10 +1,26 @@
 window.addEventListener("load", function(){
 
-    
+    let error = []    
     let name = document.querySelector("#name")
     let lastName = document.querySelector("#lastName")
     let email = document.querySelector("#email")
     let password = document.querySelector("#password")
     let avatar = document.querySelector("#avatar")
     
+    if (name.value == ""){
+        error.push("Debe tener mas de 2 caracteres")
+    }
+    if (error.length > 1){
+        event.preventDefault()
+    }
+
+    if (lastName.value == ""){
+        error.push("Debe tener mas de 2 caracteres")
+    }
+    if (error.length > 1){
+        event.preventDefault()
+    }
+
+    
+
 })
