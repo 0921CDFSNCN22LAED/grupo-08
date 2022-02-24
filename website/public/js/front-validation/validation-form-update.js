efae125dec48a888995ae38d21317f780f1f4e1a
MarcosSWeis <marcosweis01@gmail.com>
2022-02-24 14:37:48 -0300

    fixed validation update front-end

--- website/public/js/front-validation/validation-form-update.js
+++ website/public/js/front-validation/validation-form-update.js
@@ -1,156 +1,176 @@
 window.onload = function () {
-    //   const body = document.querySelector("body");
-    //   body.style.backgroundColor = "red";
-    const form = document.getElementById("form_edit_product");
-    const inputFile = document.getElementById("avatar_p_edit");
-    const msg_error_files = document.getElementById("msg_error_files");
-    const title = document.getElementById("title");
-    const error_title = document.getElementById("error_title");
-    const shortDescription = document.getElementById("shortDescription");
-    const error_shortDescription = document.getElementById(
-        "error_shortDescription"
-    );
-    const price = document.getElementById("price");
-    const error_price = document.getElementById("error_price");
-    const size = document.getElementById("size");
-    const error_size = document.getElementById("error_size");
-    const material = document.getElementById("material");
-    const error_material = document.getElementById("error_material");
-    const longDescription = document.getElementById("longDescription");
-    const error_longDescription = document.getElementById(
-        "error_longDescription"
-    );
+  const form = document.getElementById("form_edit_product");
+  const inputFile = document.getElementById("avatar_p_edit");
+  const msg_error_files = document.getElementById("msg_error_files");
+  const title = document.getElementById("title");
+  const error_title = document.getElementById("error_title");
+  const shortDescription = document.getElementById("shortDescription");
+  const error_shortDescription = document.getElementById(
+    "error_shortDescription"
+  );
+  const price = document.getElementById("price");
+  const error_price = document.getElementById("error_price");
+  const size = document.getElementById("size");
+  const error_size = document.getElementById("error_size");
+  const material = document.getElementById("material");
+  const error_material = document.getElementById("error_material");
+  const longDescription = document.getElementById("longDescription");
+  const error_longDescription = document.getElementById(
+    "error_longDescription"
+  );
 
-    let errors = {};
-    inputFile.addEventListener("change", function () {
-        const arrayFile = this.files;
-        let extensionErrors = [];
-        const extensionAcepte = ["image/gif", "image/jpg", "image/jpeg"];
-        for (element of arrayFile) {
-            if (!extensionAcepte.includes(element.type)) {
-                extensionErrors.push(element);
-            }
-        }
-        if (extensionErrors.length != 0) {
-            errors.images =
-                " debe ingresar una foto con formato valido front-end los formatos aceptados son jpg, png, gif, jpeg";
-            msg_error_files.innerText =
-                "debe ingresar una foto con formato valido front-end los formatos aceptados son jpg, png, gif, jpeg";
-            //  msg_error_files.innerText esta para que sea mas rapido , ni vien cargas la foto te salta error si no es un formato valido
-        }
-    });
-    console.log(form);
-    form.addEventListener("submit", function (e) {
-        const valueTitle = title.value;
-        console.log(valueTitle == "", "si es un string vacio");
-        if (valueTitle == "" || valueTitle.replace(/ /g, "") == "") {
-            //saco los espacios vacíos con replace
-            errors.title = "Debe ingresar un titulo front-end";
-        }
-        if (valueTitle != "" && valueTitle.length <= 5) {
-            errors.title = "El campo title debe tener al menos 5 caracteres";
-        }
-        const valueShortDescription = shortDescription.value;
-        if (
-            valueShortDescription == "" ||
-            valueShortDescription.replace(/ /g, "") == ""
-        ) {
-            errors.shortDescription = "Debe llenar el campo Breve Descripción";
-        }
+  let errors = {};
+  inputFile.addEventListener("change", function () {
+    const arrayFile = this.files;
+    let extensionErrors = [];
+    const extensionAcepte = ["image/gif", "image/jpg", "image/jpeg"];
+    for (element of arrayFile) {
+      if (!extensionAcepte.includes(element.type)) {
+        extensionErrors.push(element);
+      }
+    }
+    if (extensionErrors.length != 0) {
+      errors.images =
+        " debe ingresar una foto con formato valido front-end los formatos aceptados son jpg, png, gif, jpeg";
+      msg_error_files.innerText =
+        "debe ingresar una foto con formato valido front-end los formatos aceptados son jpg, png, gif, jpeg";
+      //  msg_error_files.innerText esta para que sea mas rapido , ni vien cargas la foto te salta error si no es un formato valido
+    }
+  });
+  console.log(form);
+  form.addEventListener("submit", function (e) {
+    const valueTitle = title.value;
+    console.log(valueTitle == "", "si es un string vacio");
+    if (valueTitle == "" || valueTitle.replace(/ /g, "") == "") {
+      //saco los espacios vacíos con replace
+      errors.title = "Debe ingresar un titulo front-end";
+    } else if (valueTitle != "" && valueTitle.length <= 5) {
+      errors.title = "El campo title debe tener al menos 5 caracteres";
+    } else {
+      if (errors.title) {
+        delete errors.title;
+      }
+    }
+    const valueShortDescription = shortDescription.value;
 
-        if (valueShortDescription.length <= 20) {
-            errors.shortDescription =
-                "El campo breve descripción debe tener al menos 20 caracteres";
-        }
-        const valuePrice = price.value;
-        if (valuePrice == "") {
-            errors.price = "Debe llenar el campo precio";
-        }
-        if (isNaN(Number(valuePrice))) {
-            //isNaN devuelve falso cundo es un numero y devuelve true cuando no lo es, por eso no lo niego
-            errors.price = "El campo precio debe ser un numero";
-        }
-        const valueSize = size.value;
+    if (
+      valueShortDescription == "" ||
+      valueShortDescription.replace(/ /g, "") == ""
+    ) {
+      errors.shortDescription = "Debe llenar el campo Breve Descripción";
+    } else if (valueShortDescription.length <= 20) {
+      errors.shortDescription =
+        "El campo breve descripción debe tener al menos 20 caracteres";
+    } else {
+      if (errors.shortDescription) {
+        delete errors.shortDescription;
+      }
+    }
+    const valuePrice = price.value;
 
-        if (valueSize == "") {
-            errors.size = "Debe seleccionar un tamaño";
-        }
-        const valueMaterial = material.value;
-        console.log(valueMaterial == "", 'material = ""');
-        if (valueMaterial == "") {
-            errors.material = "Debe seleccionar un material";
-        }
-        const valueLongDescription = longDescription.value;
-        if (
-            valueLongDescription == "" ||
-            valueLongDescription.replace(/ /g, "") == ""
-        ) {
-            errors.longDescription = "Debe llenar el campo descripción";
-        }
-        if (valueLongDescription.length <= 20) {
-            errors.longDescription =
-                "El campo descripción debe tener al menos 20 caracteres";
-        }
+    if (valuePrice == "") {
+      errors.price = "Debe llenar el campo precio";
+    } else if (isNaN(Number(valuePrice))) {
+      //isNaN devuelve falso cundo es un numero y devuelve true cuando no lo es, por eso no lo niego
+      errors.price = "El campo precio debe ser un numero";
+    } else {
+      if (errors.price) {
+        delete errors.price;
+      }
+    }
+    const valueSize = size.value;
 
-        if (Object.keys(errors) === 0) {
-            form.submit();
-        } else {
-            console.log(errors, "hay errorees");
-            e.preventDefault();
-            console.log(errors.title, "dentro del prevent");
+    if (valueSize == "") {
+      errors.size = "Debe seleccionar un tamaño";
+    } else {
+      if (errors.size) {
+        delete errors.size;
+      }
+    }
+    const valueMaterial = material.value;
+    console.log(valueMaterial == "", 'material = ""');
+    if (valueMaterial == "") {
+      errors.material = "Debe seleccionar un material";
+    } else {
+      if (errors.material) {
+        delete errors.material;
+      }
+    }
+    const valueLongDescription = longDescription.value;
 
-            if (errors.title) {
-                error_title.innerText = errors.title;
-                title.classList.add("is-invalid");
-            } else {
-                error_title.innerText = "";
-                title.classList.remove("is-invalid", "is-valid");
-            }
-            if (errors.shortDescription) {
-                error_shortDescription.innerText = errors.shortDescription;
-                shortDescription.classList.add("is-invalid");
-            } else {
-                error_shortDescription.innerText = "";
-                shortDescription.classList.remove("is-invalid", "is-valid");
-            }
+    if (
+      valueLongDescription == "" ||
+      valueLongDescription.replace(/ /g, "") == ""
+    ) {
+      errors.longDescription = "Debe llenar el campo descripción";
+    } else if (valueLongDescription.length <= 20) {
+      errors.longDescription =
+        "El campo descripción debe tener al menos 20 caracteres";
+    } else {
+      if (errors.material) {
+        delete errors.material;
+      }
+    }
 
-            if (errors.price) {
-                error_price.innerText = errors.price;
-                price.classList.add("is-invalid");
-            } else {
-                error_price.innerText = "";
-                price.classList.remove("is-invalid");
-            }
+    if (Object.keys(errors) === 0) {
+      form.submit();
+    } else {
+      console.log(errors, "hay errorees");
+      e.preventDefault();
+      console.log(errors.title, "dentro del prevent");
 
-            if (errors.size) {
-                error_size.innerText = errors.size;
-                size.classList.add("is-invalid");
-            } else {
-                error_size.innerText = "";
-                size.classList.remove("is-invalid", "is-valid");
-            }
-            if (errors.material) {
-                error_material.innerText = errors.material;
-                material.classList.add("is-invalid");
-            } else {
-                error_material.innerText = "";
-                material.classList.remove("is-invalid", "is-valid");
-            }
+      if (errors.title) {
+        error_title.innerText = errors.title;
+        title.classList.add("is-invalid");
+      } else {
+        error_title.innerText = "";
+        title.classList.remove("is-invalid");
+      }
+      if (errors.shortDescription) {
+        error_shortDescription.innerText = errors.shortDescription;
+        shortDescription.classList.add("is-invalid");
+      } else {
+        error_shortDescription.innerText = "";
+        shortDescription.classList.remove("is-invalid");
+      }
 
-            if (errors.longDescription) {
-                error_longDescription.innerText = errors.longDescription;
-                longDescription.classList.add("is-invalid");
-            } else {
-                error_longDescription.innerText = "";
-                longDescription.classList.remove("is-invalid", "is-valid");
-            }
-            if (errors.images) {
-                msg_error_files.innerText = errors.images;
-                inputFile.classList.add("is-invalid");
-            } else {
-                msg_error_files.innerText = "";
-                inputFile.classList.remove("is-invalid");
-            }
-        }
-    });
+      if (errors.price) {
+        error_price.innerText = errors.price;
+        price.classList.add("is-invalid");
+      } else {
+        error_price.innerText = "";
+        price.classList.remove("is-invalid");
+      }
+
+      if (errors.size) {
+        error_size.innerText = errors.size;
+        size.classList.add("is-invalid");
+      } else {
+        error_size.innerText = "";
+        size.classList.remove("is-invalid");
+      }
+      if (errors.material) {
+        error_material.innerText = errors.material;
+        material.classList.add("is-invalid");
+      } else {
+        error_material.innerText = "";
+        material.classList.remove("is-invalid");
+      }
+
+      if (errors.longDescription) {
+        error_longDescription.innerText = errors.longDescription;
+        longDescription.classList.add("is-invalid");
+      } else {
+        error_longDescription.innerText = "";
+        longDescription.classList.remove("is-invalid");
+      }
+      if (errors.images) {
+        msg_error_files.innerText = errors.images;
+        inputFile.classList.add("is-invalid");
+      } else {
+        msg_error_files.innerText = "";
+        inputFile.classList.remove("is-invalid");
+      }
+    }
+  });
 };
