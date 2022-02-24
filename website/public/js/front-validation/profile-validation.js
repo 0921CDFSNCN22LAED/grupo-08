9a64bb5eb3376fd4311c11e4c1cd19d401d208bd
Esteban Larcher <estebanlarcher098@gmail.com>
2022-02-24 19:20:32 -0300

    validations front

--- website/public/js/front-validation/profile-validation.js
+++ website/public/js/front-validation/profile-validation.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     const form = document.querySelector("#formProfile");
     const userName = document.querySelector("#profileName");
-    const blankLastName = document.querySelector("#profileLastName");
+    const lastName = document.querySelector("#profileLastName");
     const nameError = document.querySelector("#errorName");
-    const errorLastName = document.querySelector("#errorLastName");
+    const LastNameError = document.querySelector("#errorLastName");
 
     form.addEventListener("submit", function (e) {
         let errors = {};
@@ -11,6 +11,7 @@ window.onload = function () {
         if (userName.value == "") {
             errors.name = "El nombre no puede estar vacio";
         }
+
         //object.keys recorre el objeto completo
         if (Object.keys(errors) === 0) {
             form.submit();
@@ -21,5 +22,18 @@ window.onload = function () {
         }
         //eliminar propiedad de un objeto
         delete errors.name;
+
+        if (lastName.value == "") {
+            errors.name = "El nombre no puede estar vacio";
+        }
+        if (Object.keys(errors) === 0) {
+            form.submit();
+        } else if (Object.keys(errors) != 0) {
+            e.preventDefault();
+            console.log(errors);
+            LastNameError.innerText = errors.lastName;
+        } else {
+            delete errors.lastName;
+        }
     });
 };
