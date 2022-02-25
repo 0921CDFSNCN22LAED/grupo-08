381483a720b4e698542f32d41c1a71442e66ff22
Esteban Larcher <estebanlarcher098@gmail.com>
2022-02-24 21:14:33 -0300

    validations errors

--- website/public/js/front-validation/profile-validation.js
+++ website/public/js/front-validation/profile-validation.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     const form = document.querySelector("#formProfile");
     const userName = document.querySelector("#profileName");
-    const lastName = document.querySelector("#profileLastName");
     const nameError = document.querySelector("#errorName");
-    const LastNameError = document.querySelector("#errorLastName");
+    //const lastName = document.querySelector("#userLastName");
+    //const lastNameError = document.querySelector("#errorLastName");
 
     form.addEventListener("submit", function (e) {
         let errors = {};
@@ -11,29 +11,17 @@ window.onload = function () {
         if (userName.value == "") {
             errors.name = "El nombre no puede estar vacio";
         }
-
+        //--------------------------------------------------------------------//
         //object.keys recorre el objeto completo
-        if (Object.keys(errors) === 0) {
-            form.submit();
-        } else if (Object.keys(errors) != 0) {
+        if (Object.keys(errors) != 0) {
             e.preventDefault();
             console.log(errors);
             nameError.innerText = errors.name;
-        }
-        //eliminar propiedad de un objeto
-        delete errors.name;
-
-        if (lastName.value == "") {
-            errors.name = "El nombre no puede estar vacio";
-        }
-        if (Object.keys(errors) === 0) {
+        } else if (errors.name) {
+            //eliminar propiedad de un objeto
+            delete errors.name;
             form.submit();
-        } else if (Object.keys(errors) != 0) {
-            e.preventDefault();
-            console.log(errors);
-            LastNameError.innerText = errors.lastName;
-        } else {
-            delete errors.lastName;
         }
+        //--------------------------------------------------------------------//
     });
 };
