6dd7561fdf82d5d86edff1aaa73b0653e4d8c6db
segundogosio <segundogosio@gmail.com>
2022-03-01 09:53:07 -0300

    validaciones fron register

---
+++ website/public/js/front-validation/register.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", function(){
+
+    const error = []    
+    const form = document.getElementById("formRegister")
+    const name = document.getElementById("name")
+    const lastName = document.getElementById("lastName")
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+    const avatar = document.getElementById("avatar")
+    const erroresName = document.getElementById("erroresName")
+    const erroresLastName = document.getElementById("erroresLastName")
+    const erroresPasword = document.getElementById("erroresPasword")
+    const errorsImages = document.getElementById("errorsImages")
+    console.log(form)
+    console.log(name)
+
+
+    form.addEventListener("submit", function(event){
+        let extensionErrors = [];
+        const extensionAcepte = ["image/gif", "image/jpg", "image/jpeg"];
+        
+
+    if (name.value != "" && name.value <= 2){
+        error.push("a")
+        erroresName.innerText = "Debe llenar por lo menos 2 caracteres. front"
+    }
+    if (lastName.value != "" && lastName.value <= 2){
+        error.push("a")
+        erroresLastName.innerText = "Debe llenar por lo menos 2 caracteres. front"
+    }
+    if (password.value != "" && password.value <= 8){
+        error.push("a")
+        erroresPasword.innerText = "Debe llenar por lo menos 8 caracteres. front"
+    }
+    for (element of avatar) {
+            if (!extensionAcepte.includes(element.type)) {
+                extensionErrors.push(element);
+            }
+        }
+    if (extensionErrors.length != 0) {
+            errorsImages.innerText =
+            " debe ingresar una foto con formato valido front-end los formatos aceptados son jpg, png, gif, jpeg";
+            
+        }
+
+    })
+    
+})
