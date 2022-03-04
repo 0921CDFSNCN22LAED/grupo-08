2ab9f7d0b747cdd187f43e05932f77d2510e9245
Esteban Larcher <estebanlarcher098@gmail.com>
2022-03-03 21:57:56 -0300

    validation user profile

--- website/public/js/front-validation/user-profile.js
+++ website/public/js/front-validation/user-profile.js
@@ -1,43 +1,42 @@
 window.onload = function () {
-<<<<<<< HEAD
-  //   const body = document.querySelector("body");
-  //   body.style.backgroundColor = "red";
-  const formDeleteAccount = document.getElementById("deleteAccount");
-  const containerForm = document.getElementById("containerForm");
-  containerForm.classList.add("none");
-  const btnEnviar = document.getElementById("btnEnviarProfile");
-  btnEnviar.classList.add("none");
+    //   const body = document.querySelector("body");
+    //   body.style.backgroundColor = "red";
+    const formDeleteAccount = document.getElementById("deleteAccount");
+    const containerForm = document.getElementById("containerForm");
+    containerForm.classList.add("none");
+    const btnEnviar = document.getElementById("btnEnviarProfile");
+    btnEnviar.classList.add("none");
 
-  const btnEdit = document.getElementById("btnEditProfile");
-  btnEdit.addEventListener("click", () => {
-    const formNone = containerForm.classList.contains("none");
-    const enviarNone = btnEnviar.classList.contains("none");
-    //if ternario
-    formNone
-      ? containerForm.classList.replace("none", "block")
-      : containerForm.classList.replace("block", "none");
-    //if ternario
-    enviarNone
-      ? btnEnviar.classList.replace("none", "block")
-      : btnEnviar.classList.replace("block", "none");
-  });
-  console.log(swal);
-  formDeleteAccount.addEventListener("submit", (event) => {
-    event.preventDefault();
-    swal(
-      "Esta seguro que desea eliminar su cuenta, no podrá volver a usarla?",
-      {
-        dangerMode: true,
-        buttons: true,
-      }
-    ).then((response) => {
-      console.log(response);
-      if (response) {
-        formDeleteAccount.submit();
-      }
+    const btnEdit = document.getElementById("btnEditProfile");
+    btnEdit.addEventListener("click", () => {
+        const formNone = containerForm.classList.contains("none");
+        const enviarNone = btnEnviar.classList.contains("none");
+        //if ternario
+        formNone
+            ? containerForm.classList.replace("none", "block")
+            : containerForm.classList.replace("block", "none");
+        //if ternario
+        enviarNone
+            ? btnEnviar.classList.replace("none", "block")
+            : btnEnviar.classList.replace("block", "none");
     });
-  });
-=======
+    console.log(swal);
+    formDeleteAccount.addEventListener("submit", (event) => {
+        event.preventDefault();
+        swal(
+            "Esta seguro que desea eliminar su cuenta, no podrá volver a usarla?",
+            {
+                dangerMode: true,
+                buttons: true,
+            }
+        ).then((response) => {
+            console.log(response);
+            if (response) {
+                formDeleteAccount.submit();
+            }
+        });
+    });
+
     const form = document.querySelector("#formProfile");
     const userName = document.querySelector("#profileName");
     const nameError = document.querySelector("#errorName");
@@ -45,6 +44,7 @@ window.onload = function () {
     const lastNameError = document.querySelector("#errorLastName");
     const adress = document.querySelector("#adress");
     const adressError = document.querySelector("#adressError");
+
     const PORT = `http://localhost:3001/`;
 
     async function getPassInDb() {
@@ -55,52 +55,57 @@ window.onload = function () {
     }
 
     form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
         let errors = {};
 
         if (userName.value == "") {
             errors.name = "El nombre no puede estar vacio";
+        } else {
+            if (errors.name) {
+                delete errors.name;
+                nameError.innerText = "";
+            }
         }
-        //--------------------------------------------------------------------//
-        //object.keys recorre el objeto completo
-        if (Object.keys(errors) != 0) {
-            e.preventDefault();
-            console.log(errors);
-            nameError.innerText = errors.name;
-        } else if (errors.name) {
-            //eliminar propiedad de un objeto
-            delete errors.name;
-            form.submit();
-        }
-        //--------------------------------------------------------------------//
+
         if (lastName.value == "") {
             errors.lastname = "El apellido no puede estar vacio";
+        } else {
+            if (errors.lastname) {
+                delete errors.lastname;
+                lastNameError.innerText = "";
+            }
         }
 
-        if (Object.keys(errors) != 0) {
-            e.preventDefault();
-            console.log(errors);
-            lastNameError.innerText = errors.lastname;
-        } else if (errors.lastname) {
-            //eliminar propiedad de un objeto
-            delete errors.lastname;
-            form.submit();
-        }
-
-        //--------------------------------------------------------------------//
         if (adress.value == "" || adress.value.length < 6) {
             errors.adress =
                 "La direccion debe contener mas de 5 caracteres y no puede estar vacia";
+        } else {
+            if (errors.adress) {
+                delete errors.adress;
+                adressError.innerText = "";
+            }
         }
-
-        if (Object.keys(errors) != 0) {
-            e.preventDefault();
-            console.log(errors);
-            adressError.innerText = errors.adress;
-        } else if (errors.adress) {
-            //eliminar propiedad de un objeto
-            delete errors.adress;
+        console.log(errors);
+        if (Object.keys(errors) == 0) {
             form.submit();
+            console.log("No se encontraron errores");
+        } else {
+            if (errors.name) {
+                nameError.innerText = errors.name;
+            }
+            if (errors.lastname) {
+                lastNameError.innerText = errors.lastname;
+            }
+            if (errors.adress) {
+                adressError.innerText = errors.adress;
+            }
+        }
+        //--------------------------------------------------------------------//
+        const userPassword = getPassInDb();
+        if (userPassword) {
+            errors.password;
+            console.log(errors);
         }
     });
->>>>>>> e7cc994c8ce20dad33368a4dbdcf0f661118b525
 };
--- website/src/controllers/api/user-api-controller.js
+++ website/src/controllers/api/user-api-controller.js
@@ -5,83 +5,83 @@
 const { getListUsers, getUserById } = require("../../services/api/apiUser");
 
 module.exports = {
-  list: async (req, res) => {
-    try {
-      const users = await getListUsers();
-      let status;
-      let statusCode;
-      if (users) {
-        status = true;
-        statusCode = 200;
-      } else {
-        status = false;
-        statusCode = 404;
-      }
-      const response = {
-        meta: {
-          status: status,
-          total: users.length,
-          statusCode: statusCode,
-          url: "/api/users",
-        },
-        data: users,
-      };
-      res.json(response);
-    } catch (errors) {
-      console.log(errors);
-    }
-  },
-  detail: async (req, res) => {
-    try {
-      const user = await getUserById(req.params.userId);
-      let status;
-      let statusCode;
-      if (user) {
-        status = true;
-        statusCode = 200;
-      } else {
-        status = false;
-        statusCode = 404;
-      }
-      const response = {
-        meta: {
-          status: status,
-          total: user.length,
-          statusCode: statusCode,
-          url: `/api/users/${user.id}`,
-        },
-        data: user,
-      };
-      res.json(response);
-    } catch (errors) {
-      console.log(errors);
-    }
-  },
+    list: async (req, res) => {
+        try {
+            const users = await getListUsers();
+            let status;
+            let statusCode;
+            if (users) {
+                status = true;
+                statusCode = 200;
+            } else {
+                status = false;
+                statusCode = 404;
+            }
+            const response = {
+                meta: {
+                    status: status,
+                    total: users.length,
+                    statusCode: statusCode,
+                    url: "/api/users",
+                },
+                data: users,
+            };
+            res.json(response);
+        } catch (errors) {
+            console.log(errors);
+        }
+    },
+    detail: async (req, res) => {
+        try {
+            const user = await getUserById(req.params.userId);
+            let status;
+            let statusCode;
+            if (user) {
+                status = true;
+                statusCode = 200;
+            } else {
+                status = false;
+                statusCode = 404;
+            }
+            const response = {
+                meta: {
+                    status: status,
+                    total: user.length,
+                    statusCode: statusCode,
+                    url: `/api/users/${user.id}`,
+                },
+                data: user,
+            };
+            res.json(response);
+        } catch (errors) {
+            console.log(errors);
+        }
+    },
     getUserValidation: async (req, res) => {
-    try {
-      const query = req.query.userDB
-      const users = await getUserValidation(query);
-      let status;
-      let statusCode;
-      if (users) {
-        status = true;
-        statusCode = 200;
-      } else {
-        status = false;
-        statusCode = 404;
-      }
-      const response = {
-        meta: {
-          status: status,
-          total: users.length,
-          statusCode: statusCode,
-          url: "/api/users",
-        },
-        data: users,
-      };
-      res.json(response);
-    } catch (errors) {
-      console.log(errors);
-    }
-  },
+        try {
+            const query = req.query.userDB;
+            const users = await getUserValidation(query);
+            let status;
+            let statusCode;
+            if (users) {
+                status = true;
+                statusCode = 200;
+            } else {
+                status = false;
+                statusCode = 404;
+            }
+            const response = {
+                meta: {
+                    status: status,
+                    total: users.length,
+                    statusCode: statusCode,
+                    url: "/api/users",
+                },
+                data: users,
+            };
+            res.json(response);
+        } catch (errors) {
+            console.log(errors);
+        }
+    },
 };
