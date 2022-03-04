62c61b7ab16031249eb8c1990109b0a65dfbc1eb
MarcosSWeis <marcosweis01@gmail.com>
2022-03-04 20:12:26 -0300

    mitad de las validacione front register

--- website/public/js/user/register.js
+++ website/public/js/user/register.js
@@ -1,154 +1,90 @@
 window.addEventListener("load", function () {
   const errors = {};
-  const form = document.querySelector("#formRegister");
-  const name = document.querySelector("#name");
-  const errorsName = document.querySelector("#errorsName");
-  const lastName = document.querySelector("#lastName");
-  const erroesLastName = document.querySelector("#erroesLastName");
-  const password = document.querySelector("#password");
-  const erroresPasword = document.querySelector("#erroresPasword");
-  const email = document.querySelector("#email");
-  const erroresEmail = document.querySelector("#erroresEmail");
+  const form = document.getElementById("formRegister");
+  const name = document.getElementById("name");
+  const errorsName = document.getElementById("errorsName");
+  const lastName = document.getElementById("lastName");
+  const errorLastName = document.getElementById("errorLastName");
+  const password = document.getElementById("password");
+  const errorPassword = document.getElementById("errorPassword");
+  const email = document.getElementById("email");
+  const erroresEmail = document.getElementById("erroresEmail");
   const PORT = `http://localhost:3001/`;
   async function getUserInDb(email) {
-    const userDb = await fetch(
+    const response = await fetch(
       `${PORT}api/users/validationUser?userDB=${email}`
     );
-    return userDb;
+    const data = await response.json();
+    return data;
   }
 
   form.addEventListener("submit", (e) => {
-    console.log(email.value);
-    getUserInDb(email.value).then((response) => {
-      console.log(response);
-      if (response) {
-        errors.email = "este email no puede repetirse";
-      } else {
-        if (errors.email) {
-          //eliminar propiedad de un objeto
-          delete errors.email;
-          erroresEmail.innerText = "";
-        }
-      }
-    });
-    // console.log(userDB);
-    if (name.value == "" || name.value.length <= 2) {
-      errors.name = "Debe tener mas de 2 caracteres";
+    e.preventDefault();
+    if (name.value == "") {
+      errors.name = "El campo Nombre no puede estar vació Front";
     } else {
       if (errors.name) {
-        //eliminar propiedad de un objeto
         delete errors.name;
         errorsName.innerText = "";
       }
+      if (name.value.length <= 2) {
+        errors.name = "El campo Nombre debe tener al menos 2 caracteres Front";
+      } else {
+        if (errors.name) {
+          delete errors.name;
+          errorsName.innerText = "";
+        }
+      }
     }
 
-    if (lastName.value == "" || lastName.value.length <= 2) {
-      errors.lastName = "Debe tener mas de 2 caracteres";
+    if (lastName.value == "") {
+      errors.lastName = "El campo apellido no puede estar vació Front";
     } else {
       if (errors.name) {
-        //eliminar propiedad de un objeto
         delete errors.lastName;
-        erroesLastName.innerText = "";
+        errorLastName.innerText = "";
+      }
+      if (lastName.value.length <= 2) {
+        errors.lastName =
+          "Debe campo apellido tener al menos 2 caracteres  Front";
+      } else {
+        if (errors.name) {
+          delete errors.lastName;
+          errorLastName.innerText = "";
+        }
       }
     }
 
-    // console.log(email.value.indexOf("@") != -1, 4654545465);
-    // if (email.value == "" || email.value.indexOf("@") != -1) {
-    //   errors.email = "Debe ser un email";
-    // } else {
-    //   if (errors.email) {
-    //     //eliminar propiedad de un objeto
-    //     delete errors.email;
-    //     erroresEmail.innerText = "";
-    //   }
-    // }
-
-    if (password.value == "" || password.value.length <= 8) {
-      errors.password = "Debe tener mas de 8 caracteres";
+    if (password.value == "") {
+      errors.password = "El campo contraseña no puede estar vació Front";
     } else {
       if (errors.password) {
-        //eliminar propiedad de un objeto
         delete errors.password;
-        erroresPasword.innerText = "";
+        errorPassword.innerText = "";
       }
-    }
-    console.log(errors);
-
-    if (lastName.value == "" || lastName.value.length <= 2) {
-      errors.lastName = "Debe tener mas de 2 caracteres";
-    } else {
-      if (errors.lastName) {
-        //eliminar propiedad de un objeto
-        delete errors.lastName;
-        erroesLastName.innerText = "";
-      }
-    }
-
-    const user = getUserInDb(email.value);
-    console.log(user);
-    if (user) {
-      errors.email;
-    }
-
-    /* console.log(email.value.indexOf(".com") == -1)
-        if (email.value == "" || email.value.indexOf(".com") == -1){
-            errors.email = "Debe ser un email"
-
-        } else {
-           if (errors.email) { 
-            //eliminar propiedad de un objeto
-            delete errors.email;
-            erroresEmail.innerText = ""
-        }} */
-
-    if (password.value == "" || password.value.length <= 8) {
-      errors.password = "Debe tener mas de 8 caracteres";
-    } else {
-      if (errors.password) {
-        //eliminar propiedad de un objeto
-        delete errors.password;
-        erroresPasword.innerText = "";
+      if (password.value.length <= 8) {
+        errors.password =
+          "El campo contraseña debe tener al menos 8 caracteres Front";
+      } else {
+        if (errors.password) {
+          delete errors.password;
+          errorPassword.innerText = "";
+        }
       }
     }
 
-    const extensionAcepte = ["image/gif", "image/jpg", "image/jpeg"];
-
-    if (images.value.indexOf == extensionAcepte) {
-      console.log(images);
-      errors.images = "Formatos aceptados son jpg, png, gif, jpeg";
-    } else {
-      if (errors.images) {
-        delete errors.images;
-        errorsImages.innerText = "";
-      }
-    }
-
-    console.log(errors);
-
-    //object.keys recorre el objeto completo
     if (Object.keys(errors) == 0) {
       form.submit();
       console.log("no hubo errores");
     } else {
       if (errors.name) {
         errorsName.innerText = errors.name;
-        errorsName.style.color = "red";
       }
       if (errors.lastName) {
-        erroesLastName.innerText = errors.lastName;
-        erroesLastName.style.color = "red";
-      }
-      if (errors.email) {
-        erroresEmail.innerText = errors.email;
-        erroresEmail.style.color = "red";
+        errorLastName.innerText = errors.lastName;
       }
       if (errors.password) {
-        erroresPasword.innerText = errors.password;
-        erroresPasword.style.color = "red";
-      }
-      if (errors.images) {
-        errorsImages.innerText = errors.images;
-        errorsImages.style.color = "red";
+        errorPassword.innerText = errors.password;
       }
     }
   });
