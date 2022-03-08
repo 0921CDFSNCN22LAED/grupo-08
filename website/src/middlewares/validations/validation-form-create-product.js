bdfff8ddd0ac15100bce59910f63930a463375a2
Esteban Larcher <estebanlarcher098@gmail.com>
2022-03-08 20:44:49 -0300

    correcion errroes

--- website/src/middlewares/validations/validation-form-create-product.js
+++ website/src/middlewares/validations/validation-form-create-product.js
@@ -1,10 +1,9 @@
 const path = require("path");
 const { body } = require("express-validator");
 module.exports = [
-<<<<<<< HEAD
     body("name")
         .notEmpty()
-        .withMessage("Debe completar el campo nombre del product")
+        .withMessage("Debe completar el campo nombre del producto")
         .isLength({ min: 5 })
         .withMessage(
             "el nombre del producto debe contener al menos 5 caracteres"
@@ -13,7 +12,7 @@ module.exports = [
         .notEmpty()
         .withMessage("Debe completar le campo de breve descripción")
         .isLength({ min: 20 })
-        .withMessage("La descripción debe contener menso de 20 caracteres"),
+        .withMessage("La descripción debe contener al menos de 20 caracteres"),
     body("price")
         .notEmpty()
         .withMessage("Debe completar el campo de precio")
@@ -23,7 +22,7 @@ module.exports = [
     body("color")
         .notEmpty()
         .isLength({ max: 7 })
-        .withMessage("Debe selecciona un color"),
+        .withMessage("Debe seleccionar un color"),
     body("img_product_create").custom((values, { req }) => {
         const files = req.files;
         if (files.length == 0) {
@@ -46,48 +45,6 @@ module.exports = [
                     );
                 }
             }
-=======
-  body("name")
-    .notEmpty()
-    .withMessage("Debe completar el campo nombre del producto")
-    .isLength({ min: 5 })
-    .withMessage("el nombre del producto debe contener al menos 5 caracteres"),
-  body("shortDescription")
-    .notEmpty()
-    .withMessage("Debe completar le campo de breve descripción")
-    .isLength({ min: 20 })
-    .withMessage("La descripción debe contener al menos de 20 caracteres"),
-  body("price")
-    .notEmpty()
-    .withMessage("Debe completar el campo de precio")
-    .isNumeric()
-    .withMessage("El campo precio debe ser un numero"),
-  body("size").notEmpty().withMessage("Debe seleccionar un talle"),
-  body("color")
-    .notEmpty()
-    .isLength({ max: 7 })
-    .withMessage("Debe selecciona un color"),
-  body("img_product_create").custom((values, { req }) => {
-    const files = req.files;
-    if (files.length == 0) {
-      throw new Error("Debe subir al menos una imagen ");
-    } else {
-      //doble verificacion de formato
-      const files = req.files;
-      const extensionAcepted = [
-        "image/jpeg",
-        "image/jpg",
-        "image/gif",
-        "image/png",
-      ];
-      for (image of files) {
-        if (!extensionAcepted.includes(image.mimetype)) {
-          // lo niego, false implica que es un formato valido
-          // entonces true significa ques no es un formato invalido
-          throw new Error(
-            `Debe subir una foto con formato valido, los formatos aceptados son  ${extensionAcepted}`
-          );
->>>>>>> 75cf42fb88a913b84c4635f5ed1f147f28d14a9e
         }
         return true;
     }),
